93807f091fea3fcbf683718d6c474714d77a0a52
Chris Choi <chutsu@gmail.com>
2018-11-26 11:08:55 +0000

    Update calib_stereo_test.cpp

--- tests/calib/calib_stereo_test.cpp
+++ tests/calib/calib_stereo_test.cpp
@@ -6,7 +6,8 @@ namespace prototype {
 
 #define IMAGE_DIR "/data/euroc_mav/cam_april/mav0/cam0/data"
 #define APRILGRID_CONF "test_data/calib/aprilgrid/target.yaml"
-#define APRILGRID_DATA "/tmp/aprilgrid_test"
+#define CAM0_APRILGRID_DATA "/tmp/aprilgrid_test/cam0"
+#define CAM1_APRILGRID_DATA "/tmp/aprilgrid_test/cam1"
 
 void test_setup() {
   // Setup calibration target
@@ -25,7 +26,7 @@ void test_setup() {
                                       image_size,
                                       lens_hfov,
                                       lens_vfov,
-                                      APRILGRID_DATA);
+                                      CAM0_APRILGRID_DATA);
   if (retval == -1) {
     FATAL("Failed to preprocess camera data!");
   }
@@ -34,10 +35,16 @@ void test_setup() {
 int test_stereo_residual() {
   // Test load
   std::vector<aprilgrid_t> cam0_aprilgrids;
-  int retval = load_camera_calib_data(APRILGRID_DATA, cam0_aprilgrids);
+  int retval = load_camera_calib_data(CAM0_APRILGRID_DATA, cam0_aprilgrids);
   MU_CHECK(retval == 0);
   MU_CHECK(cam0_aprilgrids.size() > 0);
   MU_CHECK(cam0_aprilgrids[0].ids.size() > 0);
+
+  std::vector<aprilgrid_t> cam1_aprilgrids;
+  retval = load_camera_calib_data(CAM1_APRILGRID_DATA, cam1_aprilgrids);
+  MU_CHECK(retval == 0);
+  MU_CHECK(cam1_aprilgrids.size() > 0);
+  MU_CHECK(cam1_aprilgrids[0].ids.size() > 0);
 
   // Setup intrinsic and distortion initialization
   const vec2_t image_size{752, 480};
@@ -48,15 +55,15 @@ int test_stereo_residual() {
   const double cam0_fy = pinhole_focal_length(image_size(1), lens_vfov);
   const double cam0_cx = image_size(0) / 2.0;
   const double cam0_cy = image_size(1) / 2.0;
-  const double cam0_intrinsics[4] = {cam0_fx, cam0_fy, cam0_cx, cam0_cy};
-  const double cam0_distortion[4] = {0.01, 0.0001, 0.0001, 0.0001};
+  const vec4_t cam0_intrinsics{cam0_fx, cam0_fy, cam0_cx, cam0_cy};
+  const vec4_t cam0_distortion{0.01, 0.0001, 0.0001, 0.0001};
   // -- cam1 intrinsics and distortion
   const double cam1_fx = pinhole_focal_length(image_size(0), lens_hfov);
   const double cam1_fy = pinhole_focal_length(image_size(1), lens_vfov);
   const double cam1_cx = image_size(0) / 2.0;
   const double cam1_cy = image_size(1) / 2.0;
-  const double cam1_intrinsics[4] = {cam1_fx, cam1_fy, cam1_cx, cam1_cy};
-  const double cam1_distortion[4] = {0.01, 0.0001, 0.0001, 0.0001};
+  const vec4_t cam1_intrinsics{cam1_fx, cam1_fy, cam1_cx, cam1_cy};
+  const vec4_t cam1_distortion{0.01, 0.0001, 0.0001, 0.0001};
 
   // Setup cam0 cam1 extrinsics
   // clang-format off
@@ -67,46 +74,59 @@ int test_stereo_residual() {
                  0.0, 0.0, 0.0, 1.0;
   // clang-format on
 
-  // for (size_t i = 0; i < cam0_aprilgrids.size(); i++) {
-  //   // Setup
-  //   const auto grid = cam0_aprilgrids[i];
-  //   const auto tag_id = grid.ids[0];
-  //   const int corner_id = 0;
-  //   const auto cam0_kp = grid.keypoints[0];
-  //   const auto T_CF = grid.T_CF;
-  //
-  //   // Get object point
-  //   vec3_t p_F;
-  //   if (aprilgrid_object_point(grid, tag_id, corner_id, p_F) != 0) {
-  //     LOG_ERROR("Failed to calculate AprilGrid object point!");
-  //   }
-  //
-  //   // Form residual and call the functor
-  //   // -- Pose of fiducial expressed in cam0
-  //   const quat_t q_C0F{T_C0F.block<3, 3>(0, 0)};
-  //   const vec3_t t_C0F{T_C0F.block<3, 1>(0, 3)};
-  //   const double q_C0F_data[4] = {q_C0F.x(), q_C0F.y(), q_C0F.z(), q_C0F.w()};
-  //   const double t_C0F_data[3] = {t_C0F(0), t_C0F(1), t_C0F(2)};
-  //   // -- Pose of fiducial expressed in cam0
-  //   double result[4] = {0.0, 0.0, 0.0, 0.0};
-  //   const stereo_residual_t residual{cam0_kp, cam1_kp, p_F};
-  //   residual(cam0_intrinsics,
-  //            cam0_distortion,
-  //            cam1_intrinsics,
-  //            cam1_distortion,
-  //            q_C0C1,
-  //            t_C0C1,
-  //            q_C0F,
-  //            t_C0F,
-  //            result);
-  //
-  //   // Just some arbitrary test to make sure reprojection error is not larger
-  //   // than 100pixels in x or y direction. But often this can be the case ...
-  //   MU_CHECK(result[0] < 100.0);
-  //   MU_CHECK(result[1] < 100.0);
-  //   MU_CHECK(result[2] < 100.0);
-  //   MU_CHECK(result[3] < 100.0);
-  // }
+  for (size_t i = 0; i < cam0_aprilgrids.size(); i++) {
+    // AprilGrid, keypoint and relative pose observed in cam0
+    const auto &grid0 = cam0_aprilgrids[i];
+    const auto &cam0_kp = grid0.keypoints[0];
+    const auto &T_C0F = grid0.T_CF;
+
+    // AprilGrid, keypoint and relative pose observed in cam1
+    const auto &grid1 = cam1_aprilgrids[i];
+    const auto &cam1_kp = grid1.keypoints[0];
+    const auto &T_C1F = grid1.T_CF;
+
+    // Tag id and corner id
+    const auto tag_id = grid0.ids[0];
+    const int corner_id = 0;
+
+    // Form pose of fiducial expressed in cam0
+    const quat_t q_C0F{T_C0F.block<3, 3>(0, 0)};
+    const vec3_t t_C0F{T_C0F.block<3, 1>(0, 3)};
+
+    // Form cam0-cam1 extrinsics
+    const quat_t q_C0C1{1.0, 0.0, 0.0, 0.0};
+    const vec3_t t_C0C1{0.0, 0.0, 0.0};
+
+    // Form residual and call the functor
+    // -- Get the object point
+    vec3_t p_F;
+    if (aprilgrid_object_point(grid0, tag_id, corner_id, p_F) != 0) {
+      LOG_ERROR("Failed to calculate AprilGrid object point!");
+    }
+    // -- Form residual
+    const stereo_residual_t residual{cam0_kp, cam1_kp, p_F};
+
+    // Calculate residual
+    double result[4] = {0.0, 0.0, 0.0, 0.0};
+    residual(cam0_intrinsics.data(),
+             cam0_distortion.data(),
+             cam1_intrinsics.data(),
+             cam1_distortion.data(),
+             q_C0C1.coeffs().data(),
+             t_C0C1.data(),
+             q_C0F.coeffs().data(),
+             t_C0F.data(),
+             result);
+
+    std::cout << result[0] << " " << result[1] << " " << result[2] << " " << result[3] << std::endl;
+
+    // Just some arbitrary test to make sure reprojection error is not larger
+    // than 100pixels in x or y direction. But often this can be the case ...
+    MU_CHECK(result[0] < 100.0);
+    MU_CHECK(result[1] < 100.0);
+    MU_CHECK(result[2] < 100.0);
+    MU_CHECK(result[3] < 100.0);
+  }
 
   return 0;
 }
