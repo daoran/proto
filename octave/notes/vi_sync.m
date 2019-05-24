b208d8fc0beb32d055ad7e792e63611363f37e38
Chris Choi <chutsu@gmail.com>
2019-05-24 15:06:48 +0100

    Refactor plot functions

--- octave/notes/vi_sync.m
+++ octave/notes/vi_sync.m
@@ -7,7 +7,7 @@ function x = lerp(x0, x1, t)
   x = (1 - t) * x0 + t * x1;
 endfunction
 
-function timeline = create_timeline()
+function timeline = timeline_create()
   % Sensor rates
   cam0_hz = 20.0;
   cam1_hz = 20.0;
@@ -97,178 +97,323 @@ function timeline = create_timeline()
   endfor
 endfunction
 
+function [cam0_ts] = timeline_flatten_cam0_ts(timeline)
+  cam0_type = 0;
+  cam1_type = 1;
+  accel0_type = 2;
+  gyro0_type = 3;
 
+  cam0_ts = [];
 
-% Interpolate data as it arrives
-data = {};
+  for i = 1:length(timeline)
+    ts = timeline(i).ts;
+    event_type = timeline(i).type;
+    for j = 1:length(event_type)
+      if event_type(j) == cam0_type
+        cam0_ts = [cam0_ts, ts];
+      endif
+    endfor
+  endfor
+endfunction
 
-cam0_type = 0;
-cam1_type = 1;
-accel0_type = 2;
-gyro0_type = 3;
+function [cam1_ts] = timeline_flatten_cam1_ts(timeline)
+  cam0_type = 0;
+  cam1_type = 1;
+  accel0_type = 2;
+  gyro0_type = 3;
 
-accel0_started = false;
-gyro0_started = false;
-imu_started = false;
+  cam1_ts = [];
 
-accel0_t0 = {};
-gyro0_t0 = {};
+  for i = 1:length(timeline)
+    ts = timeline(i).ts;
+    event_type = timeline(i).type;
+    for j = 1:length(event_type)
+      if event_type(j) == cam1_type
+        cam1_ts = [cam1_ts, ts];
+      endif
+    endfor
+  endfor
+endfunction
 
-cam0_ts = [];
-cam1_ts = [];
-accel0_ts = [];
-accel0_data = [];
-gyro0_ts = [];
-gyro0_data = [];
+function [accel0_ts] = timeline_flatten_accel0_ts(timeline)
+  cam0_type = 0;
+  cam1_type = 1;
+  accel0_type = 2;
+  gyro0_type = 3;
 
-interp_buf = [];
+  accel0_ts = [];
 
+  for i = 1:length(timeline)
+    ts = timeline(i).ts;
+    event_type = timeline(i).type;
+    for j = 1:length(event_type)
+      if event_type(j) == accel0_type
+        accel0_ts = [accel0_ts, ts];
+      endif
+    endfor
+  endfor
+endfunction
 
-timeline = create_timeline();
-for i = 1:length(timeline)
-% for i = 1:10
-	ts = timeline(i).ts;
-	event_type = timeline(i).type;
+function [gyro0_ts] = timeline_flatten_gyro0_ts(timeline)
+  cam0_type = 0;
+  cam1_type = 1;
+  accel0_type = 2;
+  gyro0_type = 3;
 
-	for j = 1:length(event_type)
-    % cam0 event handler
-		if event_type(j) == cam0_type && imu_started
-      cam0_ts = [cam0_ts, ts];
-		endif
+  gyro0_ts = [];
 
-    % cam1 event handler
-		if event_type(j) == cam1_type && imu_started
-      cam1_ts = [cam1_ts, ts];
-		endif
+  for i = 1:length(timeline)
+    ts = timeline(i).ts;
+    event_type = timeline(i).type;
+    for j = 1:length(event_type)
+      if event_type(j) == gyro0_type
+        gyro0_ts = [gyro0_ts, ts];
+      endif
+    endfor
+  endfor
+endfunction
 
-    % Accel event handler
-		if event_type(j) == accel0_type
-			if accel0_started == false
-				accel0_started = true;
+function [accel0_data] = timeline_flatten_accel0_data(timeline)
+  cam0_type = 0;
+  cam1_type = 1;
+  accel0_type = 2;
+  gyro0_type = 3;
 
-        accel0_t0 = {};
-        accel0_t0.ts = ts;
-        accel0_t0.data = timeline(i).accel0;
-			endif
+  accel0_data = [];
 
-      % Interpolate accel0
-      for i = 1:length(interp_buf)
-        interp_ts = interp_buf(i);
+  for i = 1:length(timeline)
+    event_type = timeline(i).type;
+    for j = 1:length(event_type)
+      if event_type(j) == accel0_type
+        accel0_data = [accel0_data, timeline(i).accel0];
+      endif
+    endfor
+  endfor
+endfunction
 
-        % Set lerp end point
-        accel0_t1 = {};
-        accel0_t1.ts = ts;
-        accel0_t1.data = timeline(i).accel0;
+function [gyro0_data] = timeline_flatten_gyro0_data(timeline)
+  cam0_type = 0;
+  cam1_type = 1;
+  accel0_type = 2;
+  gyro0_type = 3;
 
-        % Lerp
-        if (interp_ts - accel0_t0.ts) > 0
-          t = (interp_ts - accel0_t0.ts) / (accel0_t1.ts - accel0_t0.ts);
-          accel0_lerped = lerp(accel0_t0.data, accel0_t1.data, t);
-        else
-          accel0_lerped = accel0_t0.data;
+  gyro0_data = [];
+
+  for i = 1:length(timeline)
+    event_type = timeline(i).type;
+    for j = 1:length(event_type)
+      if event_type(j) == gyro0_type
+        gyro0_data = [gyro0_data, timeline(i).gyro0];
+      endif
+    endfor
+  endfor
+endfunction
+
+function data = synchronize_measurements(timeline)
+  % Interpolate data as it arrives
+  cam0_type = 0;
+  cam1_type = 1;
+  accel0_type = 2;
+  gyro0_type = 3;
+
+  accel0_started = false;
+  gyro0_started = false;
+  imu_started = false;
+
+  accel0_t0 = {};
+  gyro0_t0 = {};
+
+  cam0_ts = [];
+  cam1_ts = [];
+  accel0_ts = [];
+  accel0_data = [];
+  gyro0_ts = [];
+  gyro0_data = [];
+
+  interp_buf = [];
+
+  for i = 1:length(timeline)
+  % for i = 1:10
+    ts = timeline(i).ts;
+    event_type = timeline(i).type;
+
+    for j = 1:length(event_type)
+      % cam0 event handler
+      if event_type(j) == cam0_type && imu_started
+        cam0_ts = [cam0_ts, ts];
+      endif
+
+      % cam1 event handler
+      if event_type(j) == cam1_type && imu_started
+        cam1_ts = [cam1_ts, ts];
+      endif
+
+      % Accel event handler
+      if event_type(j) == accel0_type
+        if accel0_started == false
+          accel0_started = true;
+
+          accel0_t0 = {};
+          accel0_t0.ts = ts;
+          accel0_t0.data = timeline(i).accel0;
         endif
 
-        % Shift end lerp point to start
-        accel0_t0 = accel0_t1;
+        % Interpolate accel0
+        % reset_interp_buf = false;
+        for i = 1:length(interp_buf)
+          interp_ts = interp_buf(i);
+
+          % Set lerp end point
+          accel0_t1 = {};
+          accel0_t1.ts = ts;
+          accel0_t1.data = timeline(i).accel0;
+
+          % Lerp
+          if (interp_ts - accel0_t0.ts) > 0
+            t = (interp_ts - accel0_t0.ts) / (accel0_t1.ts - accel0_t0.ts);
+            accel0_lerped = lerp(accel0_t0.data, accel0_t1.data, t);
+          else
+            accel0_lerped = accel0_t0.data;
+          endif
+
+          % Shift end lerp point to start
+          accel0_t0 = accel0_t1;
+
+          % Add to buffer
+          accel0_ts = [accel0_ts, interp_ts];
+          accel0_data = [accel0_data, accel0_lerped];
+          % reset_interp_buf = true;
+        endfor
+
+        % % Reset interpolation buffer
+        % if reset_interp_buf
+        %   interp_buf = [];
+        % endif
 
         % Add to buffer
-        accel0_ts = [accel0_ts, interp_ts];
-        accel0_data = [accel0_data, accel0_lerped];
-      endfor
+        accel0_ts = [accel0_ts, ts];
+        accel0_data = [accel0_data, timeline(i).accel0];
+      endif
 
-      % Add to buffer
-      accel0_ts = [accel0_ts, ts];
-      accel0_data = [accel0_data, timeline(i).accel0];
-		endif
+      % Gyro event handler
+      if event_type(j) == gyro0_type
+        if gyro0_started == false
+          gyro0_started = true;
+        endif
 
-    % Gyro event handler
-		if event_type(j) == gyro0_type
-			if gyro0_started == false
-				gyro0_started = true;
-			endif
+        if accel0_ts(end) != ts
+          interp_buf = [interp_buf, ts];
+        endif
 
-			interp_buf = [interp_buf, ts];
+        % Add to buffer
+        gyro0_ts = [gyro0_ts, ts];
+        gyro0_data = [gyro0_data, timeline(i).gyro0];
+      endif
 
-      % Add to buffer
-      gyro0_ts = [gyro0_ts, ts];
-      gyro0_data = [gyro0_data, timeline(i).gyro0];
-    endif
+      if gyro0_started && accel0_started
+        imu_started = true;
+      endif
+    endfor
+  endfor
 
-		if gyro0_started && accel0_started
-			imu_started = true;
-		endif
-	endfor
-endfor
+  % Form return
+  data = {};
+  data.cam0_ts = cam0_ts;
+  data.cam1_ts = cam1_ts;
+  data.accel0_ts = accel0_ts;
+  data.accel0_data = accel0_data;
+  data.gyro0_ts = gyro0_ts;
+  data.gyro0_data = gyro0_data;
+endfunction
 
+function plot_timestamps(plot_title, cam0_ts, cam1_ts, accel0_ts, gyro0_ts)
+  % -- Sensors
+  cam0_plot = 0.25;
+  cam1_plot = 0.2;
+  accel0_plot = 0.15;
+  gyro0_plot = 0.10;
+  cam0_y = cam0_plot * ones(length(cam0_ts), 1);
+  cam1_y = cam1_plot * ones(length(cam1_ts), 1);
+  accel0_y = accel0_plot * ones(length(accel0_ts), 1);
+  gyro0_y = gyro0_plot * ones(length(gyro0_ts), 1);
+  % -- Plot sensor timestamps
+  hold on;
+  plot(cam0_ts, cam0_y, "rs", "linewidth", 2.0, "markerfacecolor", "r");
+  plot(cam1_ts, cam1_y, "rs", "linewidth", 2.0, "markerfacecolor", "r");
+  plot(accel0_ts, accel0_y, "bs", "linewidth", 2.0, "markerfacecolor", "b");
+  plot(gyro0_ts, gyro0_y, "gs", "linewidth", 2.0, "markerfacecolor", "g");
+  hold off;
+  % -- Plot properties
+  sensors = {"gyro0", "accel0", "cam1", "cam0"};
+  y_tick = [gyro0_plot, accel0_plot, cam1_plot, cam0_plot];
+  set(gca, "ytick", y_tick, "yticklabel", sensors);
+  xlim([0, 0.12]);
+  ylim([0, 0.3]);
+  title(plot_title);
+  xlabel("Time [s]");
+  ylabel("Sensors");
+endfunction
 
+function plot_measurements(plot_title, ts, data)
+  hold on;
+  plot(ts, data(1, :), "r-", "linewidth", 2.0);
+  title(plot_title)
+  hold off;
+endfunction
 
-figure(1);
-subplot(211);
+% MAIN
+timeline = timeline_create();
+data = synchronize_measurements(timeline);
 
-cam0_plot = 0.25;
-cam1_plot = 0.2;
-accel0_plot = 0.15;
-gyro0_plot = 0.10;
+size(data.gyro0_ts)
+size(data.accel0_ts)
 
-cam0_y = cam0_plot * ones(length(cam0_ts), 1);
-cam1_y = cam1_plot * ones(length(cam1_ts), 1);
-accel0_y = accel0_plot * ones(length(accel0_ts), 1);
-gyro0_y = gyro0_plot * ones(length(gyro0_ts), 1);
+data.gyro0_ts(1:10)
+data.accel0_ts(1:10)
 
-hold on;
-plot(cam0_ts, cam0_y, "rs", "linewidth", 2.0, "markerfacecolor", "r");
-plot(cam1_ts, cam1_y, "rs", "linewidth", 2.0, "markerfacecolor", "r");
-plot(accel0_ts, accel0_y, "bs", "linewidth", 2.0, "markerfacecolor", "b");
-plot(gyro0_ts, gyro0_y, "gs", "linewidth", 2.0, "markerfacecolor", "g");
-hold off;
-
-sensors = {"gyro0", "accel0", "cam1", "cam0"};
-y_tick = [gyro0_plot, accel0_plot, cam1_plot, cam0_plot];
-set(gca, "ytick", y_tick, "yticklabel", sensors);
-xlim([0, 0.12]);
-ylim([0, 0.3]);
-xlabel("Time [s]");
-ylabel("Sensors");
-
-
-subplot(212);
-
-hold on;
-size(gyro0_ts)
-size(gyro0_data)
-plot(gyro0_ts, gyro0_data(1, :), "r-", "linewidth", 2.0);
-
-hold off;
-
-ginput();
-
-
-% % Plot timeline
+% % Plot timestamps
 % figure(1);
+% subplot(211);
+% plot_timestamps("Un-Synchronized Measurements",
+%                 cam0_ts = timeline_flatten_cam0_ts(timeline),
+%                 cam1_ts = timeline_flatten_cam1_ts(timeline),
+%                 accel0_ts = timeline_flatten_accel0_ts(timeline),
+%                 gyro0_ts = timeline_flatten_gyro0_ts(timeline))
 %
-% cam0_plot = 0.75;
-% cam1_plot = 0.7;
-% accel0_plot = 0.25;
-% gyro0_plot = 0.2;
+% subplot(212);
+% plot_timestamps("Synchronized Measurements",
+%                 data.cam0_ts,
+%                 data.cam1_ts,
+%                 data.accel0_ts,
+%                 data.gyro0_ts);
 %
-% cam0_y = cam0_plot * ones(length(cam0_ts), 1);
-% cam1_y = cam1_plot * ones(length(cam1_ts), 1);
-% accel0_y = accel0_plot * ones(length(accel0_ts), 1);
-% gyro0_y = gyro0_plot * ones(length(gyro0_ts), 1);
-%
-% hold on;
-% plot(cam0_ts, cam0_y, "ro", "linewidth", 2.0);
-% plot(cam1_ts, cam1_y, "ro", "linewidth", 2.0);
-% plot(accel0_ts, accel0_y, "bo", "linewidth", 2.0);
-% plot(gyro0_ts, gyro0_y, "go", "linewidth", 2.0);
-% hold off;
-%
-% sensors = {"gyro0", "accel0", "cam1", "cam0"};
-% y_tick = [gyro0_plot, accel0_plot, cam1_plot, cam0_plot];
-% set(gca, "ytick", y_tick, "yticklabel", sensors);
-% xlim([0, 0.2]);
-% ylim([0, 1.0]);
-% xlabel("Time [s]");
-% ylabel("Sensors");
+% ginput();
+
+% Plot gyroscope measurements
+% figure(2);
+% subplot(211);
+% gyro0_ts = timeline_flatten_gyro0_ts(timeline);
+% gyro0_data = timeline_flatten_gyro0_data(timeline);
+% plot_measurements("Gyroscope Data", gyro0_ts, gyro0_data);
+% subplot(212);
+% gyro0_ts = data.gyro0_ts;
+% gyro0_data = data.gyro0_data;
+% plot_measurements("Interpolated Gyroscope Data", gyro0_ts, gyro0_data);
+% ginput();
+
+% % Plot accelerometer measurements
+% figure(3);
+% subplot(211);
+% accel0_ts = timeline_flatten_accel0_ts(timeline);
+% accel0_data = timeline_flatten_accel0_data(timeline);
+% size(accel0_ts)
+% size(accel0_data)
+% plot_measurements("Accelerometer Data", accel0_ts, accel0_data);
+% % subplot(212);
+% % accel0_ts = data.accel0_ts;
+% % accel0_data = data.accel0_data;
+% % size(accel0_ts)
+% % size(accel0_data)
+% % plot_measurements("Interpolated Accelerometer Data", accel0_ts, accel0_data);
 %
 % ginput();
