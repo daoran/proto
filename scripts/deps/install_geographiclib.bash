3b4d6a8fce367ef08a5a22b9f1947f06750f0b28
Chris Choi <chutsu@gmail.com>
2019-05-31 00:49:28 +0100

    Update install script for geographiclib

--- scripts/deps/install_geographiclib.bash
+++ scripts/deps/install_geographiclib.bash
@@ -3,4 +3,8 @@ set -e  # Exit on first error
 BASEDIR=$(dirname "$0")
 source $BASEDIR/config.bash
 
-apt-get install -qqq libgeographic-dev
+# apt-get install -qqq libgeographic-dev
+
+install_git_repo \
+  https://git.code.sf.net/p/geographiclib/code \
+  geographiclib
