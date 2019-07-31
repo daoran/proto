f822cc02d30ff60f1db5b7fb9db6595899ca9d15
Chris Choi <chutsu@gmail.com>
2019-07-31 17:42:15 +0100

    Put AprilTags install after OpenCV3

--- scripts/deps/install.bash
+++ scripts/deps/install.bash
@@ -10,8 +10,7 @@ mkdir -p "$DOWNLOAD_PATH"
 apt-get update -qqq
 apt-get install -qqq -y git mercurial cmake g++
 
-echo "Installing apriltags ..." && "$BASEDIR"/install_apriltags.bash
-# echo "Installing assimp ..." && "$BASEDIR"/install_assimp.bash
+echo "Installing assimp ..." && "$BASEDIR"/install_assimp.bash
 echo "Installing boost ..." && "$BASEDIR"/install_boost.bash
 echo "Installing ceres ..." && "$BASEDIR"/install_ceres.bash
 echo "Installing eigen ..." && "$BASEDIR"/install_eigen.bash
@@ -19,5 +18,6 @@ echo "Installing geographiclib ..." && "$BASEDIR"/install_geographiclib.bash
 echo "Installing glfw3 ..." && "$BASEDIR"/install_glfw3.bash
 echo "Installing glm ..." && "$BASEDIR"/install_glm.bash
 echo "Installing opencv3 ..." && "$BASEDIR"/install_opencv3.bash
+echo "Installing apriltags ..." && "$BASEDIR"/install_apriltags.bash
 echo "Installing opengl ..." && "$BASEDIR"/install_opengl.bash
 echo "Installing yamlcpp ..." && "$BASEDIR"/install_yamlcpp.bash
