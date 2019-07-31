8d2bfb58c4a217e9ff542a14d75d20525730b838
Chris Choi <chutsu@gmail.com>
2019-07-31 20:11:13 +0100

    Fix glad install script

--- scripts/deps/install_glad.bash
+++ scripts/deps/install_glad.bash
@@ -15,7 +15,7 @@ mkdir -p "$BLD_DIR"
 # Build glad
 cd "$PREFIX"/src
 apt_install python3-pip
-sudo pip3 install glad
+sudo -H pip3 install glad
 glad --quiet --generator c --out glad
 g++ -I"$INC_DIR" -fPIC -c "$SRC_DIR/glad.c" -o "$BLD_DIR/glad.o";
 ar rs "$BLD_DIR/libglad.a" "$BLD_DIR/glad.o";
