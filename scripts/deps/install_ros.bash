9306b0b37b478340a16df1f73293b39e317f0917
Chris Choi <chutsu@gmail.com>
2019-09-05 19:21:18 +0100

    Update ROS install script

--- scripts/deps/install_ros.bash
+++ scripts/deps/install_ros.bash
@@ -10,10 +10,11 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 sudo apt-key adv \
   --keyserver 'hkp://keyserver.ubuntu.com:80' \
   --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt-get update
-sudo apt-get install dpkg
+apt_update
+apt_install dpkg
 sudo dpkg --configure -a
-sudo apt-get install ros-melodic-desktop
-sudo apt-get install python-catkin-tools
+apt_install ros-melodic-desktop
+apt_install python-catkin-tools
+apt_install ros-melodic-gazebo-dev*
 sudo rosdep init
 rosdep update
