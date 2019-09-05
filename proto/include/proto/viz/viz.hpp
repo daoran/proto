fb8144abd8ebf17ebcc546647943180839264283
Chris Choi <chutsu@gmail.com>
2019-09-05 19:45:39 +0100

    Add viz.hpp

---
+++ proto/include/proto/viz/viz.hpp
@@ -0,0 +1,13 @@
+#ifndef PROTOTOYE_VIZ_VIZ_HPP
+#define PROTOTOYE_VIZ_VIZ_HPP
+
+// #include "proto/viz/camera.hpp"
+// #include "proto/viz/common.hpp"
+// #include "proto/viz/draw.hpp"
+// #include "proto/viz/gui.hpp"
+// #include "proto/viz/mesh.hpp"
+// #include "proto/viz/model.hpp"
+// #include "proto/viz/shader.hpp"
+// #include "proto/viz/texture.hpp"
+
+#endif // PROTOTYPE_VIZ_VIZ_HPP
