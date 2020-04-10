c34caa491067cd21d4a6d31c3e07f26781ee4a36
Chris Choi <chutsu@gmail.com>
2020-04-10 13:02:42 +0100

    Add acceleration to circle_sim.m

--- octave/notes/circle_sim.m
+++ octave/notes/circle_sim.m
@@ -1,6 +1,7 @@
 circle_r = 5.0;
 nb_samples = 100;
-theta = -pi;
+% theta = -pi;
+theta = 1.0;
 
 sim_time = [];
 x_pos = [];
@@ -10,16 +11,22 @@ y_vel = [];
 x_acc = [];
 y_acc = [];
 
-dt = 0.1;
+dt = 0.001;
 t_end = 10.0;
 
 f = 1.0 / 10.0;
 w = 2.0 * pi * f;
 
-x = circle_r * cos(theta);
-y = circle_r * sin(theta);
-vx = -circle_r * sin(theta) * w;
-vy = circle_r * cos(theta) * w;
+theta_k = theta;
+theta_km1 = theta - (w * dt);
+theta_km2 = theta_km1 - (w * dt);
+
+ax = -circle_r * w**2 * cos(theta);
+ay = -circle_r * w**2 * sin(theta);
+vx = -circle_r * w * sin(theta);
+vy = circle_r * w * cos(theta);
+x = circle_r * cos(theta_k);
+y = circle_r * sin(theta_k);
 
 for t = 0.0:dt:t_end;
   sim_time = [sim_time; t];
@@ -27,29 +34,54 @@ for t = 0.0:dt:t_end;
   y_pos = [y_pos; y];
   x_vel = [x_vel; vx];
   y_vel = [y_vel; vy];
+  x_acc = [x_acc; ax];
+  y_acc = [y_acc; ay];
 
   # Update theta
   theta += w * dt;
 
-  # Update velocity
-  vx = -circle_r * sin(theta) * w;
-  vy = circle_r * cos(theta) * w;
-
   # Update position
   x = x + vx * dt;
   y = y + vy * dt;
+
+  # Update velocity
+  vx = vx + ax * dt;
+  vy = vy + ay * dt;
+
+  # Update acceleration
+  ax = -circle_r * w**2 * cos(theta);
+  ay = -circle_r * w**2 * sin(theta);
 endfor
 
 figure(1);
-subplot(211);
-hold on;
 plot(x_pos, y_pos);
-axis("equal");
+xlabel("x [m]");
+ylabel("y [m]");
+% xlim([-circle_r, circle_r]);
+% ylim([-circle_r, circle_r]);
 
-subplot(212);
+figure(2);
+subplot(311);
+hold on;
+plot(sim_time, x_pos, 'r-', 'linewidth', 2.0);
+plot(sim_time, y_pos, 'g-', 'linewidth', 2.0);
+xlabel("Time [s]");
+ylabel("Displacement [m]");
+
+subplot(312);
 hold on;
 plot(sim_time, x_vel, 'r-', 'linewidth', 2.0);
 plot(sim_time, y_vel, 'g-', 'linewidth', 2.0);
+xlabel("Time [s]");
+ylabel("Velocity [ms^{-1}]");
+
+subplot(313);
+hold on;
+plot(sim_time, x_acc, 'r-', 'linewidth', 2.0);
+plot(sim_time, y_acc, 'g-', 'linewidth', 2.0);
+xlabel("Time [s]");
+ylabel("Acceleration [ms^{-2}]");
+
 ginput();
 
 % figure(1);
