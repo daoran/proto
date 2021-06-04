667d60f8416e2385e174e3228f16ae6504aa8d6a
Chris Choi <chutsu@gmail.com>
2021-06-04 13:35:04 +0100

    Fix glfw install script

--- deps/scripts/install_glfw.bash
+++ deps/scripts/install_glfw.bash
@@ -3,9 +3,9 @@ set -e  # halt on first error
 source "config.bash"
 
 # Install deps
-sudo apt-get install libxrandr-dev
-sudo apt-get install libxinerama-dev
-sudo apt-get install libxcursor-dev
+sudo apt-get install -y libxrandr-dev
+sudo apt-get install -y libxinerama-dev
+sudo apt-get install -y libxcursor-dev
 
 # Build
 install_git_repo https://github.com/glfw/glfw glfw
