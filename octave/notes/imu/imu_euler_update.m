e0f5450c998d8ded27c0c66803bd4711d82bf2c5
Chris Choi <chutsu@gmail.com>
2021-07-24 15:10:47 +0100

    Remove old imu factor notes

--- octave/notes/imu/imu_euler_update.m
+++ octave/notes/imu/imu_euler_update.m
@@ -10,7 +10,7 @@ function x_imu = imu_euler_update(x_imu, acc, gyr, dt)
   a = acc - ba;
   dt_sq = dt * dt;
 
-  x_imu.C_WS *= Exp(w * dt);
-  x_imu.v_WS += ((C_WS_i * a) - g) * dt;
   x_imu.p_WS += (v_WS_i * dt) + (0.5 * (C_WS_i * a - g) * dt_sq);
+  x_imu.v_WS += ((C_WS_i * a) - g) * dt;
+  x_imu.C_WS *= Exp(w * dt);
 endfunction
