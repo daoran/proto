cdf5de33b7836f69bc687ae663adeb2d7fcddd57
Chris Choi <chutsu@gmail.com>
2021-08-08 16:30:27 +0100

    Update check_factor_jacobian.m

--- octave/proto/se/factor/check_factor_jacobian.m
+++ octave/proto/se/factor/check_factor_jacobian.m
@@ -3,7 +3,7 @@ function check_factor_jacobian(factor, params, param_idx, jac_name, step_size, t
   [r, jacs] = factor.eval(factor, params);
 
   % Numerical diff
-  fdiff = zeros(rows(r), params{param_idx}.min_dims);
+  J_numdiff = zeros(rows(r), params{param_idx}.min_dims);
   for i = 1:params{param_idx}.min_dims
     % Perturb and evaluate
     params_fwd = params;
@@ -19,8 +19,9 @@ function check_factor_jacobian(factor, params, param_idx, jac_name, step_size, t
     [r_fwd, _] = factor.eval(factor, params_fwd);
 
     % Forward finite difference
-    fdiff(:, i) = (r_fwd - r) / step_size;
+    J_numdiff(:, i) = (r_fwd - r) / step_size;
   endfor
 
-  check_jacobian(jac_name, fdiff, jacs{param_idx}, threshold, true);
+  J_analytical = jacs{param_idx};
+  check_jacobian(jac_name, J_numdiff, J_analytical, threshold, true);
 endfunction
