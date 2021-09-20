88ea8f1a08d86b134b8bbaee4dc8bcf1ff5d0a84
Chris Choi <chutsu@gmail.com>
2021-09-20 16:51:38 +0100

    Fix bug

--- octave/notes/inv_depth_param.m
+++ octave/notes/inv_depth_param.m
@@ -8,8 +8,8 @@ x_i = 0.0;
 y_i = 0.0;
 z_i = 0.0;
 rho_i = 1.0 / 10.0;
-theta_i = 0.01;
-phi_i = 0.02;
+theta_i = 0.1;
+phi_i = 0.2;
 
 function p_W = m(theta_i, phi_i)
   p_W = [cos(phi_i) * sin(theta_i);
@@ -22,8 +22,8 @@ p_W = [x_i; y_i; z_i] + 1 / rho_i * m(theta_i, phi_i)
 J_x_i = [1; 0; 0];
 J_y_i = [0; 1; 0];
 J_z_i = [0; 0; 1];
-J_theta_i = [cos(phi_i) * -cos(theta_i); 0.0; cos(phi_i) * sin(theta_i)];
-J_phi_i = [sin(phi_i) * sin(theta_i); cos(phi_i); sin(phi_i) * cos(theta_i)];
+J_theta_i = 1 / rho_i * [cos(phi_i) * cos(theta_i); 0.0; cos(phi_i) * -sin(theta_i)];
+J_phi_i = 1 / rho_i * [-sin(phi_i) * sin(theta_i); -cos(phi_i); -sin(phi_i) * cos(theta_i)];
 J_rho_i = -1 / rho_i**2 * m(theta_i, phi_i);
 
 J_inv_depth = [J_x_i, J_y_i, J_z_i, J_theta_i, J_phi_i, J_rho_i];
