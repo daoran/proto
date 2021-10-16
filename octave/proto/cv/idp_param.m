e851a8f486f40184ee128038eec640ebcc193e7e
Chris Choi <chutsu@gmail.com>
2021-10-16 16:54:24 +0100

    Refactor inverse depth parameterization functions

---
+++ octave/proto/cv/idp_param.m
@@ -0,0 +1,17 @@
+function param = idp_param(T_WC, z)
+  % Convert image pixel coordinates to normalized retinal coordintes
+  x = [(z(1) - cx) / fx; (z(2) - cy) / fy; 1.0];
+
+  % Convert 3D ray from camera frame to world frame
+  r_WC = tf_trans(T_WC);
+  C_WC = tf_rot(T_WC);
+  h_W = C_WC * x;
+
+  % Obtain bearing (theta, phi) and inverse depth (rho)
+  theta = atan2(h_W(1), h_W(3));
+  phi = atan2(-h_W(2), sqrt(h_W(1) * h_W(1) + h_W(3) * h_W(3)));
+  rho = 1.0;
+
+  % Form inverse depth parameter
+  param = [r_WC; theta; phi; rho];
+endfunction
--- octave/proto/cv/idp_point.m
+++ octave/proto/cv/idp_point.m
@@ -1,16 +1,20 @@
-function p_W = idp2point(param)
+function p_W = idp_point(param)
+  % Camera position in world frame
   x = param(1);
   y = param(2);
   z = param(3);
   r_WC = [x; y; z];
 
+  % Bearing angle (theta, phi) and inverse depth (rho)
   theta = param(4);
   phi = param(5);
   rho = param(6);
 
+  % Convert bearing to 3D ray from camera frame
   m = [cos(phi) * sin(theta);
        -sin(phi);
        cos(phi) * cos(theta)];
 
+  % Form 3D point in world frame
   p_W = r_WC + (1.0 / rho) * m;
 endfunction
