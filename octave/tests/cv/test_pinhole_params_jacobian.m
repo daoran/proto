4e93c278d3dc3e6ae4b17247836e39a821639b6e
Chris Choi <chutsu@gmail.com>
2021-10-18 17:51:21 +0100

    Add pinhole functions

---
+++ octave/tests/cv/test_pinhole_params_jacobian.m
@@ -0,0 +1,61 @@
+addpath(genpath("proto"));
+test_passed = true;
+
+% Camera
+img_w = 640;
+img_h = 480;
+fx = 320.0;
+fy = 320.0;
+cx = img_w / 2.0;
+cy = img_h / 2.0;
+proj_params = [fx, fy, cx, cy];
+
+% Camera pose in world frame
+C_WC = euler321([-pi / 2, 0.0, -pi / 2]);
+r_WC = [0.0; 0.0; 0.0];
+T_WC = tf(C_WC, r_WC);
+
+% 3D World point
+p_W = [10.0; 0.1; 0.2];
+
+% Point in camera frame
+p_C = tf_point(inv(T_WC), p_W);
+
+% Normalize point
+x = [p_C(1) / p_C(3);
+     p_C(2) / p_C(3)];
+
+% Project to image plane
+z = [fx * x(1) + cx; fy * x(2) + cy];
+
+% Pinhole params jacobian
+J = pinhole_params_jacobian(proj_params, x);
+
+% Perform numerical diff to obtain finite difference
+step_size = 1e-6;
+finite_diff = zeros(2, 4);
+for i = 1:4
+  params_diff = proj_params;
+  params_diff(i) += step_size;
+
+  fx = params_diff(1);
+  fy = params_diff(2);
+  cx = params_diff(3);
+  cy = params_diff(4);
+
+  z_diff = [fx * x(1) + cx; fy * x(2) + cy];
+
+  finite_diff(1:2, i) = (z_diff - z) / step_size;
+endfor
+
+% Compare numerical finite diff and analytical jacobian
+% Make sure the difference between all entries are no large than the
+% numerical diff step size
+if any(all((finite_diff - J) > step_size)) == 1
+  printf("Invalid Jacobian?\n")
+  test_passed = false;
+endif
+
+if test_passed == false
+  exit(-1);
+endif
