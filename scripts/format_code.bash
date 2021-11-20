938b026f14d19defa4832ba6f3a0decf2529f417
Chris Choi <chutsu@gmail.com>
2021-11-20 15:19:19 +0000

    Clean up

--- scripts/format_code.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-find . -name "*.cpp" -print0 | xargs --null -i  sh -c 'echo "clang-format -> {}";clang-format -style=file -i {};'
-find . -name "*.hpp" -print0 | xargs --null -i  sh -c 'echo "clang-format -> {}";clang-format -style=file -i {};'
--- scripts/run.sh
+++
@@ -1,6 +0,0 @@
-set -e
-# make release
-
-# ARDUINO=~/Downloads/arduino-1.8.13/arduino
-# $ARDUINO --upload firmware/firmware.ino --port /dev/ttyACM0
-# $ARDUINO --upload firmware/firmware.ino
