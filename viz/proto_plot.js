ec60c4dc4c1a1d565b17233894cc1bb1f83f9449
Chris Choi <chutsu@gmail.com>
2021-12-28 16:56:33 +0000

    Add docs

--- viz/proto_plot.js
+++ viz/proto_plot.js
@@ -5,6 +5,22 @@ function assert(condition, message) {
 }
 
 class PlotXY {
+  /**
+   * Construct a new X-Y plot
+   * @param plot_config JSON expected to have the following format:
+   *
+   * {
+   *     title: (str) Title of plot
+   *     width: (int) Plot width in pixels
+   *     height: (int) Plot height in pixels
+   *     div_id: (str) Div element id
+   *     buf_size: (int) Plot buffer size
+   *     traces: {name: (str), line: (JSON)}
+   *     xlabel: (str) xlabel
+   *     ylabel: (str) ylabel
+   * }
+   *
+   */
   constructor(plot_config) {
     // Plot config
     this.title = plot_config["title"];
@@ -56,6 +72,25 @@ class PlotXY {
     Plotly.newPlot(this.div_id, this.traces, this.layout, {});
   }
 
+  /**
+   * Update plot with new data.
+   *
+   * The data is expected to have the followig format:
+   *
+   *   {
+   *      <Trace-Name-1>: {"x": <xval>, "y": <yval>},
+   *      <Trace-Name-2>: {"x": <xval>, "y": <yval>}
+   *   }
+   *
+   * Example:
+   *
+   *   {
+   *      "Estimate": {"x": 0.0, "y": 0.0},
+   *      "Ground-Truth": {"x": 0.0, "y": 0.0}
+   *   }
+   *
+   * @param data JSON containing new plot data
+   */
   update(data) {
     // Process data
     var data_new = {x : [], y : []};
@@ -69,12 +104,35 @@ class PlotXY {
       }
 
       // Add new data to array
+      // Note its array into an array (its weird but thats what plotly expects)
       data_new["x"].push([ data[trace_name]["x"] ]);
       data_new["y"].push([ data[trace_name]["y"] ]);
     }
 
     // Update plot
     Plotly.extendTraces(this.div_id, data_new, this.trace_idxs, this.buf_size);
+    // Note:
+    //
+    // `data_new` should have the following format:
+    //
+    // For updating 1 trace line, the nested array is not a mistake it is what
+    // plotly expects:
+    //
+    //   {
+    //     x: [[<some-new-x-value>]]
+    //     y: [[<some-new-y-value>]]
+    //   }
+    //
+    // For updating 2 or more trace lines:
+    //
+    //   {
+    //     x: [[<some-new-x-value>], [<some-new-x-value>], ...]
+    //     y: [[<some-new-y-value>], [<some-new-y-value>], ...]
+    //   }
+    //   #     ^ for traces[0]        ^ for traces[1]      ^ for traces[n]
+    //
+    // This is why `this.trace_idxs` is significant, it tells plotly which
+    // trace indicies the `data_new` contains
 
     return true;
   }
@@ -83,9 +141,6 @@ class PlotXY {
 class MultiPlot {
   constructor() {
     this.init = false;
-    this.plot_width = 300;
-    this.plot_height = 250;
-    this.buf_size = 100;
     this.plots = {};
   }
 
@@ -127,6 +182,6 @@ function setup_session() {
   // On close
   ws.onclose = function() {
     // Retry to connect to server every 1s
-    // setTimeout(function() { setup_session(); }, 1000);
+    setTimeout(function() { setup_session(); }, 1000);
   };
 }
