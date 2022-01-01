480cfaa790d2e5ea041284c7a62cb406caaa216f
Chris Choi <chutsu@gmail.com>
2022-01-01 15:25:53 +0000

    Trim more un-used octave code

--- proto/octave/proto/cv/pinhole_params_jacobian.m
+++
@@ -1,4 +0,0 @@
-function J = pinhole_params_jacobian(proj_params, x)
-  J = [x(1), 0, 1, 0;
-       0, x(2), 0, 1];
-endfunction
--- proto/octave/proto/cv/pinhole_point_jacobian.m
+++
@@ -1,5 +0,0 @@
-function J = pinhole_point_jacobian(proj_params)
-  fx = proj_params(1);
-  fy = proj_params(2);
-  J = [fx, 0; 0, fy];
-endfunction
