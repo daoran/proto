d7a2eac3cc47676a60906ca3d193a82615536f04
Chris Choi <chutsu@gmail.com>
2022-01-11 00:01:53 +0000

    Update install apriltag

--- deps/scripts/install_apriltag.bash
+++ deps/scripts/install_apriltag.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e  # halt on first error
 source "config.bash"
-REPO_URL=https://github.com/chutsu/apriltags
+REPO_URL=https://github.com/chutsu/apriltag
 
 apt_install cmake libeigen3-dev libv4l-dev libopencv-*
-install_git_repo $REPO_URL apriltags
+install_git_repo $REPO_URL apriltag
