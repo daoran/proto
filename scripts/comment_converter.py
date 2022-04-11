286ff5a8c87e32c5b8b488b2a78f57d7b4d76395
Chris Choi <chutsu@gmail.com>
2022-04-11 01:24:45 +0100

    Update

--- scripts/comment_converter.py
+++ scripts/comment_converter.py
@@ -1,10 +1,11 @@
 #/usr/bin/evn python3
-src_file = open("proto/ubx.h", "r")
+src_file = open("proto/proto.h", "r")
 lines = src_file.readlines()
+src_file.close()
 
-new_file = open("proto/ubx.h.new", "w")
+new_file = open("proto/proto.h", "w")
 for line in lines:
-  if "/*" in line and "*/" in line:
+  if "/**" not in line and "/*" in line and "*/" in line:
     line = line.replace("/*", "//")
     line = line.replace("*/", "  ")
   new_file.write(line)
