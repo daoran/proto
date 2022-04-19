d4bb43c8832b884c7c942e2a5eb0f0d1ac2a0c96
Chris Choi <chutsu@gmail.com>
2022-04-19 14:11:34 +0100

    Update

--- firmware/firmware.h
+++ firmware/firmware.h
@@ -1,6 +1,630 @@
 #pragma once
-#include "core.h"
-#include "comms.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <assert.h>
+
+#include <Wire.h>
+#include <Arduino.h>
+
+#define UNUSED(expr)                                                           \
+  do {                                                                         \
+    (void) (expr);                                                             \
+  } while (0)
+
+inline float deg2rad(const float d) {
+  return d * (M_PI / 180.0);
+}
+
+inline float rad2deg(const float r) {
+  return r * (180.0 / M_PI);
+}
+
+inline int8_t int8(const uint8_t *data, const size_t offset) {
+  return (int8_t)(data[offset]);
+}
+
+inline uint8_t uint8(const uint8_t *data, const size_t offset) {
+  return (uint8_t)(data[offset]);
+}
+
+inline int16_t int16(const uint8_t *data, const size_t offset) {
+  return (int16_t)((data[offset + 1] << 8) | (data[offset]));
+}
+
+inline uint16_t uint16(const uint8_t *data, const size_t offset) {
+  return (uint16_t)((data[offset + 1] << 8) | (data[offset]));
+}
+
+inline int32_t int32(const uint8_t *data, const size_t offset) {
+  return (int32_t)((data[offset + 3] << 24) | (data[offset + 2] << 16) |
+                   (data[offset + 1] << 8) | (data[offset]));
+}
+
+inline uint32_t uint32(const uint8_t *data, const size_t offset) {
+  return (uint32_t)((data[offset + 3] << 24) | (data[offset + 2] << 16) |
+                    (data[offset + 1] << 8) | (data[offset]));
+}
+
+void euler321(const float euler[3], float C[3 * 3]) {
+  assert(euler != NULL);
+  assert(C != NULL);
+
+  const float phi = euler[0];
+  const float theta = euler[1];
+  const float psi = euler[2];
+
+  const float cphi = cos(phi);
+  const float sphi = sin(phi);
+  const float cpsi = cos(psi);
+  const float spsi = sin(psi);
+  const float ctheta = cos(theta);
+  const float stheta = sin(theta);
+
+  /* 1st row */
+  C[0] = cpsi * ctheta;
+  C[1] = cpsi * stheta * sphi - spsi * cphi;
+  C[2] = cpsi * stheta * cphi + spsi * sphi;
+
+  /* 2nd row */
+  C[3] = spsi * ctheta;
+  C[4] = spsi * stheta * sphi + cpsi * cphi;
+  C[5] = spsi * stheta * cphi - cpsi * sphi;
+
+  /* 3rd row */
+  C[6] = -stheta;
+  C[7] = ctheta * sphi;
+  C[8] = ctheta * cphi;
+}
+
+void quat2euler(const float q[4], float euler[3]) {
+  assert(q != NULL);
+  assert(euler != NULL);
+
+  const float qw = q[0];
+  const float qx = q[1];
+  const float qy = q[2];
+  const float qz = q[3];
+
+  const float qw2 = qw * qw;
+  const float qx2 = qx * qx;
+  const float qy2 = qy * qy;
+  const float qz2 = qz * qz;
+
+  const float t1 = atan2(2 * (qx * qw + qz * qy), (qw2 - qx2 - qy2 + qz2));
+  const float t2 = asin(2 * (qy * qw - qx * qz));
+  const float t3 = atan2(2 * (qx * qy + qz * qw), (qw2 + qx2 - qy2 - qz2));
+
+  euler[0] = t1;
+  euler[1] = t2;
+  euler[2] = t3;
+}
+
+void rot2quat(const float C[3 * 3], float q[4]) {
+  assert(C != NULL);
+  assert(q != NULL);
+
+  const float C00 = C[0];
+  const float C01 = C[1];
+  const float C02 = C[2];
+  const float C10 = C[3];
+  const float C11 = C[4];
+  const float C12 = C[5];
+  const float C20 = C[6];
+  const float C21 = C[7];
+  const float C22 = C[8];
+
+  const float tr = C00 + C11 + C22;
+  float S = 0.0f;
+  float qw = 0.0f;
+  float qx = 0.0f;
+  float qy = 0.0f;
+  float qz = 0.0f;
+
+  if (tr > 0) {
+    S = sqrt(tr + 1.0) * 2; // S=4*qw
+    qw = 0.25 * S;
+    qx = (C21 - C12) / S;
+    qy = (C02 - C20) / S;
+    qz = (C10 - C01) / S;
+  } else if ((C00 > C11) && (C[0] > C22)) {
+    S = sqrt(1.0 + C[0] - C11 - C22) * 2; // S=4*qx
+    qw = (C21 - C12) / S;
+    qx = 0.25 * S;
+    qy = (C01 + C10) / S;
+    qz = (C02 + C20) / S;
+  } else if (C11 > C22) {
+    S = sqrt(1.0 + C11 - C[0] - C22) * 2; // S=4*qy
+    qw = (C02 - C20) / S;
+    qx = (C01 + C10) / S;
+    qy = 0.25 * S;
+    qz = (C12 + C21) / S;
+  } else {
+    S = sqrt(1.0 + C22 - C[0] - C11) * 2; // S=4*qz
+    qw = (C10 - C01) / S;
+    qx = (C02 + C20) / S;
+    qy = (C12 + C21) / S;
+    qz = 0.25 * S;
+  }
+
+  q[0] = qw;
+  q[1] = qx;
+  q[2] = qy;
+  q[3] = qz;
+}
+
+void tf_trans(const float T[16], float r[3]) {
+  r[0] = T[3];
+  r[1] = T[7];
+  r[2] = T[11];
+}
+
+void tf_rot(const float T[16], float C[9]) {
+  C[0] = T[0];
+  C[1] = T[1];
+  C[2] = T[2];
+  C[3] = T[3];
+  C[4] = T[4];
+  C[5] = T[5];
+  C[6] = T[6];
+  C[7] = T[7];
+  C[8] = T[8];
+}
+
+void tf_quat(const float T[4 * 4], float q[4]) {
+  assert(T != NULL);
+  assert(q != NULL);
+  assert(T != q);
+
+  float C[3 * 3] = {0};
+  tf_rot(T, C);
+  rot2quat(C, q);
+}
+
+/* void __assert(const char *__func, */
+/*               const char *__file, */
+/*               int __lineno, const char *__sexp) { */
+/*   // transmit diagnostic informations through serial link. */
+/*   Serial.println(__func); */
+/*   Serial.println(__file); */
+/*   Serial.println(__lineno, DEC); */
+/*   Serial.println(__sexp); */
+/*   Serial.flush(); */
+/*   // abort program execution. */
+/*   abort(); */
+/* } */
+
+// GPIO /////////////////////////////////////////////////////////////////////
+
+void gpio_set_pin_mode(const int pin, const int mode) {
+  pinMode(pin, mode);
+}
+
+void gpio_digital_write(const int pin, const int mode) {
+  digitalWrite(pin, mode);
+}
+
+int gpio_digital_read(const int pin) {
+  return digitalRead(pin);
+}
+
+void gpio_analog_write(const int pin, const int mode) {
+  analogWrite(pin, mode);
+}
+
+int gpio_analog_read(const int pin) {
+  return analogRead(pin);
+}
+
+void delay_ms(const int ms) {
+  delay(ms);
+}
+
+void delay_us(const int us) {
+  delayMicroseconds(us);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+// UART //////////////////////////////////////////////////////////////////////
+
+typedef struct uart_t {
+  Stream *serial_in;
+  Print *serial_out;
+} uart_t;
+
+void uart_setup(uart_t *uart, int baud = 115200) {
+  uart->serial_in = &Serial;
+  uart->serial_out = &Serial;
+  Serial.begin(baud);
+}
+
+void uart_read_byte(const uart_t *uart, uint8_t *b) {
+  *b = uart->serial_in->read();
+}
+
+void uart_read_bytes(const uart_t *uart, uint8_t *data, const size_t len) {
+  uart->serial_in->readBytes(data, len);
+}
+
+void uart_write_byte(const uart_t *uart, const uint8_t b) {
+  uart->serial_out->write(b);
+}
+
+void uart_write_bytes(const uart_t *uart,
+                      const uint8_t *data,
+                      const size_t len) {
+  uart->serial_out->write(data, len);
+}
+
+/*
+  Simple printf for writing to an Arduino serial port.  Allows specifying
+  Serial..Serial3.
+
+  const HardwareSerial&, the serial port to use (Serial..Serial3)
+  const char* fmt, the formatting string followed by the data to be formatted
+
+      int d = 65;
+      float f = 123.4567;
+      char* str = "Hello";
+      uart_t::printf(Serial, "<fmt>", d);
+
+  Example:
+    uart_t::printf(Serial, "Sensor %d is %o and reads %1f\n", d, d, f) will
+    output "Sensor 65 is on and reads 123.5" to the serial port.
+
+  Formatting strings <fmt>
+  %B    - binary (d = 0b1000001)
+  %b    - binary (d = 1000001)
+  %c    - character (s = H)
+  %d/%i - integer (d = 65)\
+  %f    - float (f = 123.45)
+  %3f   - float (f = 123.346) three decimal places specified by %3.
+  %s    - char* string (s = Hello)
+  %X    - hexidecimal (d = 0x41)
+  %x    - hexidecimal (d = 41)
+  %%    - escaped percent ("%")
+*/
+void uart_printf(const uart_t *uart, const char *fmt, ...) {
+  va_list argv;
+  va_start(argv, fmt);
+
+  for (int i = 0; fmt[i] != '\0'; i++) {
+    if (fmt[i] == '%') {
+      // Look for specification of number of decimal places
+      int places = 2;
+      if (fmt[i + 1] >= '0' && fmt[i + 1] <= '9') {
+        places = fmt[i + 1] - '0';
+        i++;
+      }
+
+      switch (fmt[++i]) {
+        case 'B':
+          uart->serial_out->print("0b");
+        case 'b':
+          uart->serial_out->print(va_arg(argv, int), BIN);
+          break;
+        case 'c':
+          uart->serial_out->print((char) va_arg(argv, int));
+          break;
+        case 'd':
+        case 'i':
+          uart->serial_out->print(va_arg(argv, int), DEC);
+          break;
+        case 'f':
+          uart->serial_out->print(va_arg(argv, double), places);
+          break;
+        case 'l':
+          uart->serial_out->print(va_arg(argv, long), DEC);
+          break;
+        case 's':
+          uart->serial_out->print(va_arg(argv, const char *));
+          break;
+        case 'X':
+          uart->serial_out->print("0x");
+          uart->serial_out->print(va_arg(argv, int), HEX);
+          break;
+        case '%':
+          uart->serial_out->print(fmt[i]);
+          break;
+        default:
+          uart->serial_out->print("?");
+          break;
+      }
+    } else {
+      uart->serial_out->print(fmt[i]);
+    }
+  }
+  va_end(argv);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+// I2C ///////////////////////////////////////////////////////////////////////
+
+#define I2C_MAX_BUF_LEN 1024
+
+void i2c_setup() {
+  Wire.setClock(400000);
+  Wire.begin();
+}
+
+uint8_t i2c_read_byte(const uint8_t dev_addr, const uint8_t reg_addr) {
+  Wire.beginTransmission(dev_addr);
+  Wire.write(reg_addr);
+  Wire.endTransmission();
+
+  uint8_t size = 1;
+  uint8_t last = 1;
+  Wire.requestFrom(dev_addr, size, last);
+  const uint8_t data = Wire.read();
+
+  return data;
+}
+
+void i2c_read_bytes(const uint8_t dev_addr,
+                    const uint8_t reg_addr,
+                    const size_t length,
+                    uint8_t *data) {
+  Wire.beginTransmission(dev_addr);
+  Wire.write(reg_addr);
+  Wire.endTransmission();
+
+  Wire.requestFrom(dev_addr, length);
+  for (size_t i = 0; i < length; i++) {
+    data[i] = Wire.read();
+  }
+}
+
+uint8_t i2c_read_u8(const uint8_t dev_addr, const uint8_t reg_addr) {
+  return i2c_read_byte(dev_addr, reg_addr);
+}
+
+int8_t i2c_read_s8(const uint8_t dev_addr, const uint8_t reg_addr) {
+  return i2c_read_byte(dev_addr, reg_addr);
+}
+
+uint16_t i2c_read_u16(const uint8_t dev_addr, const uint8_t reg_addr) {
+  uint8_t bytes[2] = {0};
+  i2c_read_bytes(dev_addr, reg_addr, 2, bytes);
+  return uint16(bytes, 0);
+}
+
+int16_t i2c_read_s16(const uint8_t dev_addr, const uint8_t reg_addr) {
+  return i2c_read_u16(dev_addr, reg_addr);
+}
+
+uint32_t i2c_read_u32(const uint8_t dev_addr, const uint8_t reg_addr) {
+  uint8_t bytes[4] = {0};
+  i2c_read_bytes(dev_addr, reg_addr, 4, bytes);
+  return uint32(bytes, 0);
+}
+
+int32_t i2c_read_s32(const uint8_t dev_addr, const uint8_t reg_addr) {
+  return i2c_read_u32(dev_addr, reg_addr);
+}
+
+void i2c_write_byte(const uint8_t dev_addr,
+                    const uint8_t reg_addr,
+                    const uint8_t value) {
+  Wire.beginTransmission(dev_addr);
+  Wire.write(reg_addr);
+  Wire.write(value);
+  Wire.endTransmission();
+}
+
+void i2c_write_bytes(const uint8_t dev_addr,
+                     const uint8_t reg_addr,
+                     const uint8_t *data,
+                     const size_t length) {
+  Wire.beginTransmission(dev_addr);
+  Wire.write(reg_addr);
+  Wire.write(data, length);
+  Wire.endTransmission();
+}
+
+void i2c_scan_addrs(uint8_t *addrs, uint8_t *nb_addrs) {
+  *nb_addrs = 0;
+
+  for (uint8_t addr = 1; addr < 127; addr++) {
+    Wire.beginTransmission(addr);
+    if (Wire.endTransmission() == 0) {
+      addrs[*nb_addrs] = addr;
+      *nb_addrs += 1;
+    }
+  }
+}
+
+void i2c_print_addrs(uart_t *uart) {
+  uint8_t addrs[128] = {0};
+  uint8_t nb_addrs = 0;
+  i2c_scan_addrs(addrs, &nb_addrs);
+
+  uart_printf(uart, "Scan I2C devices\n");
+  uart_printf(uart, "----------------\n", nb_addrs);
+  uart_printf(uart, "nb_devices: %d\n", nb_addrs);
+  for (uint8_t i = 0; i < nb_addrs; i++) {
+    uart_printf(uart, "[%d]: %X\n", i, addrs[i]);
+  }
+  uart_printf(uart, "\n");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+//// PWM ///////////////////////////////////////////////////////////////////////
+
+typedef struct pwm_t {
+  uint8_t *pins;
+  uint8_t nb_pins;
+  int res;
+  float freq;
+  float range_max;
+  float min;
+  float max;
+} pwm_t;
+
+void pwm_setup(pwm_t *pwm,
+               uint8_t *pins,
+               const uint8_t nb_pins,
+               const int res = 15,
+               const float freq = 1000) {
+  for (uint8_t i = 0; i < nb_pins; i++) {
+    analogWriteFrequency(pins[i], freq);
+  }
+
+  float range_max = 0.0f;
+  analogWriteResolution(res);
+  switch (res) {
+    case 16:
+      range_max = 65535.0f;
+      break;
+    case 15:
+      range_max = 32767.0f;
+      break;
+    case 14:
+      range_max = 16383.0f;
+      break;
+    case 13:
+      range_max = 8191.0f;
+      break;
+    case 12:
+      range_max = 4095.0f;
+      break;
+    case 11:
+      range_max = 2047.0f;
+      break;
+    case 10:
+      range_max = 1023.0f;
+      break;
+    case 9:
+      range_max = 511.0f;
+      break;
+    case 8:
+      range_max = 255.0f;
+      break;
+    case 7:
+      range_max = 127.0f;
+      break;
+    case 6:
+      range_max = 63.0f;
+      break;
+    case 5:
+      range_max = 31.0f;
+      break;
+    case 4:
+      range_max = 15.0f;
+      break;
+    case 3:
+      range_max = 7.0f;
+      break;
+    case 2:
+      range_max = 3.0f;
+      break;
+    default:
+      range_max = 255.0f;
+      break;
+  }
+
+  pwm->pins = pins;
+  pwm->nb_pins = nb_pins;
+  pwm->res = res;
+  pwm->freq = freq;
+  pwm->range_max = range_max;
+}
+
+void pwm_set(pwm_t *pwm, const uint8_t idx, const float val) {
+  const float time_width = pwm->min + ((pwm->max - pwm->min) * val);
+  const float duty_cycle = (1.0 / time_width) / pwm->freq;
+  analogWrite(pwm->pins[idx], pwm->range_max * duty_cycle);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+// SBUS //////////////////////////////////////////////////////////////////////
+
+void sbus_setup() {
+  // The SBUS protocol uses inverted serial logic with:
+  // - Baud rate of 100000
+  // - 8 data bits
+  // - Even parity bit
+  // - 2 stop bits
+  HardwareSerial *bus = &Serial1;
+  bus->begin(100000, SERIAL_8E2);
+  bus->flush();
+}
+
+uint8_t sbus_read_byte() {
+  return Serial1.read();
+}
+
+void sbus_read(uint8_t *data, const size_t size) {
+  Serial1.readBytes(data, size);
+}
+
+void sbus_write_byte(const uint8_t data) {
+  Serial1.write(data);
+}
+
+void sbus_write(const uint8_t *data, const size_t size) {
+  Serial1.write(data, size);
+}
+
+void sbus_update(uart_t *uart) {
+  // Get sbus frame data
+  /* uint8_t frame[25] = {0}; */
+  /* frame[0] = sbus_read_byte(); */
+  /* if (frame[0] != 0x0F) { */
+  /*   uart_printf(uart, "Failed!\n"); */
+  /*   return; */
+  /* } else { */
+  /*   sbus_read(&frame[1], 24); */
+  /* } */
+  if (Serial1.available()) {
+    uart_printf(uart, "%X\n", sbus_read_byte());
+  } else {
+    uart_printf(uart, "nothing...\n");
+  }
+
+  // Parse sbus frame
+  // -- Parse flag
+  /* uint8_t frame_lost = (frame[23] & (1 << 5)); */
+  /* uint8_t failsafe_activated = (frame[23] & (1 << 4)); */
+  // -- Parse channel data
+  /* uint16_t ch[16] = {0}; */
+  /* ch[0] = ((frame[1] | frame[2] << 8) & 0x07FF); */
+  /* ch[1] = ((frame[2] >> 3 | frame[3] << 5) & 0x07FF); */
+  /* ch[2] = ((frame[3] >> 6 | frame[4] << 2 | frame[5] << 10) & 0x07FF); */
+  /* ch[3] = ((frame[5] >> 1 | frame[6] << 7) & 0x07FF); */
+  /* ch[4] = ((frame[6] >> 4 | frame[7] << 4) & 0x07FF); */
+  /* ch[5] = ((frame[7] >> 7 | frame[8] << 1 | frame[8] << 9) & 0x07FF); */
+  /* ch[6] = ((frame[9] >> 2 | frame[10] << 6) & 0x07FF); */
+  /* ch[7] = ((frame[10] >> 5 | frame[11] << 3) & 0x07FF); */
+  /* ch[8] = ((frame[12] | frame[13] << 8) & 0x07FF); */
+  /* ch[9] = ((frame[13] >> 3 | frame[14] << 5) & 0x07FF); */
+  /* ch[10] = ( (frame[14] >> 6 | frame[15] << 2 | frame[16] << 10) & 0x07FF);
+   */
+  /* ch[11] = ((frame[16] >> 1 | frame[17] << 7) & 0x07FF); */
+  /* ch[12] = ((frame[17] >> 4 | frame[18] << 4) & 0x07FF); */
+  /* ch[13] = ( (frame[18] >> 7 | frame[19] << 1 | frame[20] << 9) & 0x07FF);
+   */
+  /* ch[14] = ((frame[20] >> 2 | frame[21] << 6) & 0x07FF); */
+  /* ch[15] = ((frame[21] >> 5 | frame[22] << 3) & 0x07FF); */
+  /*  */
+  /* char ch_str[10] = {0}; */
+  /* itoa(ch[0], ch_str, 10); */
+
+  /* uart_printf(uart, "HERE!\n"); */
+  /* uart_printf(uart, "%d, ", ch[0]); */
+  /* uart_printf(uart, "%d, ", ch[1]); */
+  /* uart_printf(uart, "%d, ", ch[2]); */
+  /* uart_printf(uart, "%d\n", ch[3]); */
+
+  /* uart_printf(uart, ch_str); */
+  /* uart_printf(uart, "\r\n"); */
+}
+
+//////////////////////////////////////////////////////////////////////////////
 
 // HCSR04 ////////////////////////////////////////////////////////////////////
 
@@ -677,6 +1301,100 @@ int8_t mpu6050_set_accel_range(const mpu6050_t *imu, const int8_t range) {
   }
 
   return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+// PID CONTROLLER ////////////////////////////////////////////////////////////
+
+typedef struct pid_ctrl_t {
+  float error_prev;
+  float error_sum;
+
+  float error_p;
+  float error_i;
+  float error_d;
+
+  float kp;
+  float ki;
+  float kd;
+} pid_ctrl_t;
+
+void pid_ctrl_setup(pid_ctrl_t *pid,
+                    const float kp,
+                    const float ki,
+                    const float kd) {
+  pid->error_prev = 0.0f;
+  pid->error_sum = 0.0f;
+
+  pid->error_p = 0.0f;
+  pid->error_i = 0.0f;
+  pid->error_d = 0.0f;
+
+  pid->kp = kp;
+  pid->ki = ki;
+  pid->kd = kd;
+}
+
+float pid_ctrl_update(pid_ctrl_t *pid,
+                      const float setpoint,
+                      const float actual,
+                      const float dt) {
+  // Calculate errors
+  const float error = setpoint - actual;
+  pid->error_sum += error * dt;
+
+  // Calculate output
+  pid->error_p = pid->kp * error;
+  pid->error_i = pid->ki * pid->error_sum;
+  pid->error_d = pid->kd * (error - pid->error_prev) / dt;
+  const float output = pid->error_p + pid->error_i + pid->error_d;
+
+  pid->error_prev = error;
+  return output;
+}
+
+void pid_ctrl_reset(pid_ctrl_t *pid) {
+  pid->error_prev = 0.0;
+  pid->error_sum = 0.0;
+
+  pid->error_p = 0.0;
+  pid->error_i = 0.0;
+  pid->error_d = 0.0;
+}
+
+// FCU ///////////////////////////////////////////////////////////////////////
+
+// CONFIG
+#define HCSR04_PIN_TRIGGER 20
+#define HCSR04_PIN_ECHO 21
+#define HCSR04_MAX_DIST_CM 400
+#define HCSR04_MAX_TIMEOUT_MS 0
+
+typedef struct fcu_t {
+  // Comms
+  uart_t uart;
+
+  // Sensors
+  mpu6050_t imu;
+  bmp280_t bmp;
+  hcsr04_t uds;
+} fcu_t;
+
+void fcu_setup(struct fcu_t *fcu) {
+  // Comms
+  uart_setup(&fcu->uart);
+  i2c_setup();
+  //  sbus_setup();
+
+  // Sensors
+  mpu6050_setup(&fcu->imu);
+  bmp280_setup(&fcu->bmp);
+  hcsr04_setup(&fcu->uds,
+               HCSR04_PIN_TRIGGER,
+               HCSR04_PIN_ECHO,
+               HCSR04_MAX_DIST_CM,
+               HCSR04_MAX_TIMEOUT_MS);
 }
 
 //////////////////////////////////////////////////////////////////////////////
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,58 +1,9 @@
-#include <Wire.h>
-#include <Arduino.h>
-
-#include "core.h"
-#include "comms.h"
-#include "controls.h"
-#include "sensors.h"
-
-// FCU ///////////////////////////////////////////////////////////////////////
-
-// CONFIG
-#define HCSR04_PIN_TRIGGER 20
-#define HCSR04_PIN_ECHO 21
-#define HCSR04_MAX_DIST_CM 400
-#define HCSR04_MAX_TIMEOUT_MS 0
-
-typedef struct fcu_t {
-  // Comms
-  uart_t uart;
-
-  // Sensors
-  mpu6050_t imu;
-  bmp280_t bmp;
-  hcsr04_t uds;
-} fcu_t;
-
-void fcu_setup(struct fcu_t *fcu) {
-  // Comms
-  uart_setup(&fcu->uart);
-  i2c_setup();
-  //  sbus_setup();
-
-  // Sensors
-  mpu6050_setup(&fcu->imu);
-  bmp280_setup(&fcu->bmp);
-  hcsr04_setup(&fcu->uds,
-               HCSR04_PIN_TRIGGER,
-               HCSR04_PIN_ECHO,
-               HCSR04_MAX_DIST_CM,
-               HCSR04_MAX_TIMEOUT_MS);
-}
-
-//////////////////////////////////////////////////////////////////////////////
+#include "firmware.h"
 
 // GLOBAL VARIABLES
 fcu_t fcu;
 
-void setup() {
-  // Teensy 4.0 LED
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
-
-  // Flight controller unit
-  fcu_setup(&fcu);
-}
+// TESTS /////////////////////////////////////////////////////////////////////
 
 void test_uart() {
   uart_printf(&fcu.uart, "Hello World!\r\n");
@@ -88,15 +39,20 @@ void test_uds() {
   delay(100);
 }
 
-void loop() {
-  // i2c_print_addrs(&fcu.uart);
-  // delay(1000);
+//////////////////////////////////////////////////////////////////////////////
 
+void setup() {
+  // Teensy 4.0 LED
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  // Flight controller unit
+  fcu_setup(&fcu);
+}
+
+void loop() {
   // test_uart();
   // test_imu();
   // test_bmp();
   // test_uds();
-
-  //  sbus_update(&uart);
-  //  delay(1000);
 }
