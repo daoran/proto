eabac422710360a3e380aeeeefdfbb032f203ac4
Chris Choi <chutsu@gmail.com>
2022-07-26 13:23:57 +0100

    Update Makefile

---
+++ scripts/chrony/setup_chrony_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+sudo apt-get remove ntp ntpdate -qq
+sudo apt-get install chrony -qq
+sudo cp configs/chrony_server.conf /etc/chrony/chrony.conf
+sudo systemctl restart chrony.service
+sleep 2
+sudo systemctl --no-pager status chrony.service
+chronyc sourcestats
