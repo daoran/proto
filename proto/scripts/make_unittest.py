69d3a1895c09d861b491f352864c256b7359de30
Chris Choi <chutsu@gmail.com>
2022-10-29 15:00:58 +0100

    Add script to automatically create unittest from header

---
+++ proto/scripts/make_unittest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import argparse
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+      prog='make_unittest.py',
+      description='Makes a unittest from a single self-contained C header file',
+  )
+
+  parser.add_argument('-in', '--infile', required=True)
+  args = parser.parse_args()
+
+  guard_prefix = args.infile.replace(".h", "").upper()
+  outfile = "test_" + args.infile.replace(".h", ".c")
+
+  unittest = open(outfile, "w")
+  unittest.write(f"#define {guard_prefix}_IMPLEMENTATION\n")
+  unittest.write(f"#define {guard_prefix}_UNITTEST\n")
+  unittest.write(f"#include \"{args.infile}\"\n")
+  unittest.close()
