23a8f4791dc12f8b578c26bbc6cef973eae8ed99
Chris Choi <chutsu@gmail.com>
2023-03-03 02:00:20 +0000

    Update

--- proto/arducam.cpp
+++ proto/arducam.cpp
@@ -10,25 +10,21 @@
 int arducam_list_cameras() {
   ArduCamIndexinfo infos[16];
   const int num_cameras = ArduCam_scan(infos);
+  if (num_cameras == 0) {
+    return 0;
+  }
 
-  char serial[16];
   for (int i = 0; i < num_cameras; i++) {
     unsigned char *buf = infos[i].u8SerialNum;
-    sprintf(serial,
-            "%c%c%c%c-%c%c%c%c-%c%c%c%c",
-            buf[0],
-            buf[1],
-            buf[2],
-            buf[3],
-            buf[4],
-            buf[5],
-            buf[6],
-            buf[7],
-            buf[8],
-            buf[9],
-            buf[10],
-            buf[11]);
-    printf("index:%4d\tSerial: %s\n", infos[i].u8UsbIndex, serial);
+
+    std::ostringstream os;
+    os << buf[0] << buf[1] << buf[2] << buf[3] << "-";
+    os << buf[4] << buf[5] << buf[6] << buf[7] << "-";
+    os << buf[8] << buf[9] << buf[10] << buf[11];
+    const auto serial = os.str();
+
+    const auto cam_idx = infos[i].u8UsbIndex;
+    printf("Camera Index:%4d\tSerial: %s\n", cam_idx, serial.c_str());
   }
 
   // Need to sleep 2 seconds atleast or else for some strange reason
@@ -38,14 +34,15 @@ int arducam_list_cameras() {
   return num_cameras;
 }
 
-class MT9V034 {
+class MT9V034C {
 public:
   int index_;
   int img_w_ = 640;
   int img_h_ = 480;
-  ArduCamHandle handle;
+  std::string serial_;
+  ArduCamHandle handle_;
 
-  MT9V034(const int index) : index_{index} {
+  MT9V034C(const int index) : index_{index} {
     // Set camera configuration
     ArduCamCfg cfg = {};
     cfg.u32Width = img_w_;
@@ -58,11 +55,20 @@ public:
     cfg.u32TransLvl = 0;
 
     // Open camera
-    int ret_val = ArduCam_open(handle, &cfg, index_);
+    int ret_val = ArduCam_open(handle_, &cfg, index_);
     if (ret_val != USB_CAMERA_NO_ERROR) {
       printf("FAIL ret_val: %d\n", ret_val);
       return;
     }
+
+    // Read camera serial number
+    unsigned char buf[16];
+    ArduCam_readUserData(handle_, 0x400 - 16, 16, buf);
+    std::ostringstream os;
+    os << buf[0] << buf[1] << buf[2] << buf[3] << "-";
+    os << buf[4] << buf[5] << buf[6] << buf[7] << "-";
+    os << buf[8] << buf[9] << buf[10] << buf[11];
+    serial_ = os.str();
 
     // Set USB-Shield configurations
     // VRCMD = 0xD7, 0x4600, 0x0100, 1, 0x85
@@ -75,81 +81,49 @@ public:
     uint8_t cmd = 0xD7;
     uint8_t buf_all[7][3] = {{0x85}, {0x00}, {0xC0}, {0x40}, {0x00}, {0x02}};
     uint8_t buf_usb2[3] = {0x03, 0x04, 0x0C};
-    ArduCam_setboardConfig(handle, cmd, 0x4600, 0x0100, 1, buf_all[0]);
-    ArduCam_setboardConfig(handle, cmd, 0x4600, 0x0200, 1, buf_all[1]);
-    ArduCam_setboardConfig(handle, cmd, 0x4600, 0x0300, 1, buf_all[2]);
-    ArduCam_setboardConfig(handle, cmd, 0x4600, 0x0300, 1, buf_all[3]);
-    ArduCam_setboardConfig(handle, cmd, 0x4600, 0x0400, 1, buf_all[4]);
-    ArduCam_setboardConfig(handle, cmd, 0x4600, 0x0A00, 1, buf_all[5]);
-    ArduCam_setboardConfig(handle, cmd, 0, 0, 3, buf_usb2);
+    ArduCam_setboardConfig(handle_, cmd, 0x4600, 0x0100, 1, buf_all[0]);
+    ArduCam_setboardConfig(handle_, cmd, 0x4600, 0x0200, 1, buf_all[1]);
+    ArduCam_setboardConfig(handle_, cmd, 0x4600, 0x0300, 1, buf_all[2]);
+    ArduCam_setboardConfig(handle_, cmd, 0x4600, 0x0300, 1, buf_all[3]);
+    ArduCam_setboardConfig(handle_, cmd, 0x4600, 0x0400, 1, buf_all[4]);
+    ArduCam_setboardConfig(handle_, cmd, 0x4600, 0x0A00, 1, buf_all[5]);
+    ArduCam_setboardConfig(handle_, cmd, 0, 0, 3, buf_usb2);
 
-    // Set MT9V034 camera configurations
-    ArduCam_writeSensorReg(handle, 0x03, 480);
-    ArduCam_writeSensorReg(handle, 0x04, 640);
-    ArduCam_writeSensorReg(handle, 0x0D, 0x320);
+    // Set MT9V034C camera configurations
+    ArduCam_writeSensorReg(handle_, 0x03, img_h_);
+    ArduCam_writeSensorReg(handle_, 0x04, img_w_);
+    ArduCam_writeSensorReg(handle_, 0x0D, 0x320);
 
     // Set Hardware-trigger mode
-    int retval = ArduCam_setMode(handle, EXTERNAL_TRIGGER_MODE);
+    int retval = ArduCam_setMode(handle_, EXTERNAL_TRIGGER_MODE);
     if (retval == USB_BOARD_FW_VERSION_NOT_SUPPORT_ERROR) {
       printf("Usb board firmware version not support single mode.\n");
       printf("Fail!\n");
     }
-
-    // Read camera serial number
-    unsigned char buf[16];
-    ArduCam_readUserData(handle, 0x400 - 16, 16, buf);
-    printf("Serial: %c%c%c%c-%c%c%c%c-%c%c%c%c\n",
-           buf[0],
-           buf[1],
-           buf[2],
-           buf[3],
-           buf[4],
-           buf[5],
-           buf[6],
-           buf[7],
-           buf[8],
-           buf[9],
-           buf[10],
-           buf[11]);
   }
 
-  virtual ~MT9V034() {
-    ArduCam_close(handle);
+  virtual ~MT9V034C() {
+    ArduCam_close(handle_);
   }
 
-  void imshow(cv::Mat &image) const {
-    char name[50] = {0};
-    sprintf(name, "ArduCam%d", index_);
-
-    cv::cvtColor(image, image, cv::COLOR_BayerRG2BGR);
-    cv::resize(image,
-               image,
-               cv::Size(640, 480),
-               (0, 0),
-               (0, 0),
-               cv::INTER_LINEAR);
-    cv::imshow(name, image);
-    cv::waitKey(1);
-  }
-
-  void update() {
-    const auto retval = ArduCam_isFrameReady(handle);
+  int update(cv::Mat &image) {
+    const auto retval = ArduCam_isFrameReady(handle_);
 #ifdef USE_SOFT_TRIGGER
     if (retval != 1) {
-      ArduCam_softTrigger(handle);
-      return;
+      ArduCam_softTrigger(handle_);
+      return -1;
     }
 #endif
 
     ArduCamOutData *frame = NULL;
-    if (ArduCam_getSingleFrame(handle, frame) != USB_CAMERA_NO_ERROR) {
-      return;
+    if (ArduCam_getSingleFrame(handle_, frame) != USB_CAMERA_NO_ERROR) {
+      return -1;
     }
 
     const auto bytes = frame->pu8ImageData;
     const auto bit_width = frame->stImagePara.u8PixelBits;
 
-    cv::Mat image = cv::Mat(img_h_, img_w_, CV_8UC1);
+    image = cv::Mat(img_h_, img_w_, CV_8UC1);
     unsigned char *tmp = (unsigned char *) malloc(img_w_ * img_h_);
     int idx = 0;
     for (int i = 0; i < img_w_ * img_h_ * 2; i += 2) {
@@ -158,17 +132,33 @@ public:
     memcpy(image.data, tmp, img_w_ * img_h_);
     free(tmp);
 
-    imshow(image);
+    return 0;
   }
 };
 
+void imshow(cv::Mat &image) {
+  char name[50] = {0};
+  sprintf(name, "ArduCam%d", 0);
+
+  cv::cvtColor(image, image, cv::COLOR_BayerRG2BGR);
+  cv::resize(image,
+             image,
+             cv::Size(640, 480),
+             (0, 0),
+             (0, 0),
+             cv::INTER_LINEAR);
+  cv::imshow(name, image);
+  cv::waitKey(1);
+}
+
 int main(int argc, char **argv) {
   const auto camera_num = arducam_list_cameras();
-  printf("Found %d devices.\n", camera_num);
-
-  MT9V034 camera{0};
+  MT9V034C camera{0};
+  cv::Mat image;
   while (true) {
-    camera.update();
+    if (camera.update(image) == 0) {
+      imshow(image);
+    }
   }
 
   return 0;
