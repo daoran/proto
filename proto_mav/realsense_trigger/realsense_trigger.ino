71f2ac8111fe09e5a3a4771ffeafb7ce56207b0e
Chris Choi <chutsu@gmail.com>
2024-03-09 13:40:54 +0000

    Update realsense trigger arduino sketch

--- proto_mav/realsense_trigger/realsense_trigger.ino
+++ proto_mav/realsense_trigger/realsense_trigger.ino
@@ -1,6 +1,7 @@
 const int pwm_pin = 0;
-const int period_on_us = 100;
-const int period_off_us = 10000 - period_on_us;
+const int period_on_us = 110;
+//const int period_off_us = 100000 - period_on_us;
+const int period_off_us = 2000 - period_on_us;
 int led_state = 0;
 
 void setup() {
