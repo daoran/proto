7bf5fb2c3501d1ecef38cc1592af619f9292cb3e
Chris Choi <chutsu@gmail.com>
2024-08-04 16:39:38 +0100

    Add Arrow3D to proto.py

--- proto/proto.py
+++ proto/proto.py
@@ -550,7 +550,8 @@ from numpy import rad2deg
 from math import pi
 from math import isclose
 from math import sqrt
-# from math import floor
+from math import ceil
+from math import floor
 from math import cos
 from math import sin
 from math import tan
@@ -2294,6 +2295,36 @@ import matplotlib.pylab as plt
 import matplotlib.patches
 import matplotlib.transforms
 from mpl_toolkits import mplot3d
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
+from mpl_toolkits.mplot3d.proj3d import proj_transform
+from matplotlib.patches import FancyArrowPatch
+
+
+class Arrow3D(FancyArrowPatch):
+  def __init__(self, x, y, z, dx, dy, dz, *args, **kwargs):
+    super().__init__((0, 0), (0, 0), *args, **kwargs)
+    self._xyz = (x, y, z)
+    self._dxdydz = (dx, dy, dz)
+
+  def draw(self, renderer):
+    x1, y1, z1 = self._xyz
+    dx, dy, dz = self._dxdydz
+    x2, y2, z2 = (x1 + dx, y1 + dy, z1 + dz)
+
+    xs, ys, zs = proj_transform((x1, x2), (y1, y2), (z1, z2), self.axes.M)
+    self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+    super().draw(renderer)
+
+  def do_3d_projection(self, renderer=None):
+    x1, y1, z1 = self._xyz
+    dx, dy, dz = self._dxdydz
+    x2, y2, z2 = (x1 + dx, y1 + dy, z1 + dz)
+
+    xs, ys, zs = proj_transform((x1, x2), (y1, y2), (z1, z2), self.axes.M)
+    self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+
+    return np.min(zs)
 
 
 def plot_set_axes_equal(ax):
