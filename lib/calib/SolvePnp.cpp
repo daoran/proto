2448d70a573f833ad1e093ce6ce812a8acc34ab2
Chris Choi <chutsu@gmail.com>
2025-10-12 13:04:04 +0100

    Move static function into source

--- lib/calib/SolvePnp.cpp
+++ lib/calib/SolvePnp.cpp
@@ -64,4 +64,25 @@ int SolvePnp::estimate(const Vec2s &keypoints,
   return 0;
 }
 
+int SolvePnP::estimate(const std::shared_ptr<CameraGeometry> &camera_geometry,
+                       const std::shared_ptr<CalibTarget> &calib_target,
+                       Mat4 &T_camera_target) {
+  // Get calibration target measurements
+  std::vector<int> tag_ids;
+  std::vector<int> corner_indicies;
+  Vec2s keypoints;
+  Vec3s object_points;
+  calib_target->getMeasurements(tag_ids,
+                                corner_indicies,
+                                keypoints,
+                                object_points);
+  if (keypoints.size() < 10) {
+    return -1;
+  }
+
+  // Estimate relative pose
+  SolvePnp pnp{camera_geometry};
+  return pnp.estimate(keypoints, object_points, T_camera_target);
+}
+
 } // namespace xyz
--- lib/calib/SolvePnp.hpp
+++ lib/calib/SolvePnp.hpp
@@ -23,26 +23,10 @@ public:
                const Vec3s &object_points,
                Mat4 &T_camera_object);
 
+  /** Estimate relative pose */
   static int estimate(const std::shared_ptr<CameraGeometry> &camera_geometry,
                       const std::shared_ptr<CalibTarget> &calib_target,
-                      Mat4 &T_camera_target) {
-    // Get calibration target measurements
-    std::vector<int> tag_ids;
-    std::vector<int> corner_indicies;
-    Vec2s keypoints;
-    Vec3s object_points;
-    calib_target->getMeasurements(tag_ids,
-                                  corner_indicies,
-                                  keypoints,
-                                  object_points);
-    if (keypoints.size() < 10) {
-      return -1;
-    }
-
-    // Estimate relative pose
-    SolvePnp pnp{camera_geometry};
-    return pnp.estimate(keypoints, object_points, T_camera_target);
-  }
+                      Mat4 &T_camera_target);
 };
 
 } // namespace xyz
