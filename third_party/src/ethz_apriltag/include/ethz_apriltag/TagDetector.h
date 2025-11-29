498ea8ed76bb5f54fc38fef9faa59c4249dc0c07
Chris Choi <chutsu@gmail.com>
2025-11-29 12:56:19 +0000

    Add Kalibr's AprilTag detector

---
+++ third_party/src/ethz_apriltag/include/ethz_apriltag/TagDetector.h
@@ -0,0 +1,29 @@
+#ifndef TAGDETECTOR_H
+#define TAGDETECTOR_H
+
+#include <vector>
+
+#include "opencv2/opencv.hpp"
+
+#include "ethz_apriltag//TagDetection.h"
+#include "ethz_apriltag//TagFamily.h"
+#include "ethz_apriltag//FloatImage.h"
+
+namespace ethz_apriltag {
+
+class TagDetector {
+public:
+
+	TagFamily thisTagFamily;
+
+	//! Constructor
+  // note: TagFamily is instantiated here from TagCodes
+	TagDetector(const TagCodes& tagCodes, const size_t blackBorder=2) : thisTagFamily(tagCodes, blackBorder) {}
+
+	std::vector<TagDetection> extractTags(const cv::Mat& image);
+
+};
+
+} // namespace
+
+#endif
