bb30c0eea74adc1c2136d11b427b52d78dfc7f37
Chris Choi <chutsu@gmail.com>
2025-11-30 12:51:53 +0000

    Update

--- third_party/src/ethz_apriltag/include/ethz_apriltag/TagDetector.h
+++ third_party/src/ethz_apriltag/include/ethz_apriltag/TagDetector.h
@@ -5,23 +5,24 @@
 
 #include "opencv2/opencv.hpp"
 
-#include "ethz_apriltag//TagDetection.h"
-#include "ethz_apriltag//TagFamily.h"
-#include "ethz_apriltag//FloatImage.h"
+#include "ethz_apriltag/TagDetection.h"
+#include "ethz_apriltag/TagFamily.h"
+#include "ethz_apriltag/FloatImage.h"
+#include "ethz_apriltag/Tag36h11.h"
 
 namespace ethz_apriltag {
 
 class TagDetector {
 public:
+  TagFamily thisTagFamily;
 
-	TagFamily thisTagFamily;
-
-	//! Constructor
+  //! Constructor
   // note: TagFamily is instantiated here from TagCodes
-	TagDetector(const TagCodes& tagCodes, const size_t blackBorder=2) : thisTagFamily(tagCodes, blackBorder) {}
+  TagDetector(const TagCodes &tagCodes = tagCodes36h11,
+              const size_t blackBorder = 2)
+      : thisTagFamily(tagCodes, blackBorder) {}
 
-	std::vector<TagDetection> extractTags(const cv::Mat& image);
-
+  std::vector<TagDetection> extractTags(const cv::Mat &image);
 };
 
 } // namespace
