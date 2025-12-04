bc114868016cd03d45345129d9c97a86a9c6bc0c
Chris Choi <chutsu@gmail.com>
2025-12-04 16:11:08 +0000

    Update

---
+++ lib/calib/AprilGridConfig.cpp
@@ -0,0 +1,64 @@
+#include "AprilGridConfig.hpp"
+
+namespace xyz {
+
+void AprilGridConfig::getGridIndex(const int tag_id, int &i, int &j) const {
+  if ((tag_id - tag_id_offset) > (tag_rows * tag_cols)) {
+    FATAL("tag_id > (tag_rows * tag_cols)!");
+  } else if (tag_id < 0) {
+    FATAL("tag_id < 0!");
+  }
+
+  i = int((tag_id - tag_id_offset) / tag_cols);
+  j = int((tag_id - tag_id_offset) % tag_cols);
+}
+
+Vec3 AprilGridConfig::getObjectPoint(const int tag_id,
+                                     const int corner_index) const {
+  // Calculate the AprilGrid index using tag id
+  int i = 0;
+  int j = 0;
+  getGridIndex(tag_id, i, j);
+
+  // Calculate the x and y of the tag origin (bottom left corner of tag)
+  // relative to grid origin (bottom left corner of entire grid)
+  const double x = j * (tag_size + tag_size * tag_spacing);
+  const double y = i * (tag_size + tag_size * tag_spacing);
+
+  // Calculate the x and y of each corner
+  Vec3 object_point;
+  switch (corner_index) {
+    case 0: // Bottom left
+      object_point = Vec3(x, y, 0);
+      break;
+    case 1: // Bottom right
+      object_point = Vec3(x + tag_size, y, 0);
+      break;
+    case 2: // Top right
+      object_point = Vec3(x + tag_size, y + tag_size, 0);
+      break;
+    case 3: // Top left
+      object_point = Vec3(x, y + tag_size, 0);
+      break;
+    default:
+      FATAL("Incorrect corner id [%d]!", corner_index);
+      break;
+  }
+
+  return object_point;
+}
+
+std::map<int, Vec3> AprilGridConfig::getObjectPoints() const {
+  std::map<int, Vec3> object_points;
+
+  for (int tag_id = 0; tag_id < (tag_rows * tag_cols); ++tag_id) {
+    for (int corner_index = 0; corner_index < 4; ++corner_index) {
+      const int point_id = tag_id * 4 + corner_index;
+      object_points[point_id] = getObjectPoint(tag_id, corner_index);
+    }
+  }
+
+  return object_points;
+}
+
+} // namespace xyz
