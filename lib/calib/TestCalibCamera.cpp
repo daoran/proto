23e95a377f2efa4ee8dd53914c0b034f6065c45e
Chris Choi <chutsu@gmail.com>
2025-12-10 18:36:01 +0000

    Fix TestCalibCamera

--- lib/calib/TestCalibCamera.cpp
+++ lib/calib/TestCalibCamera.cpp
@@ -7,22 +7,40 @@
 
 namespace xyz {
 
-// TEST(CalibCamera, initializeIntrinsics) {
-//   CalibCamera calib{TEST_CONFIG};
-//   calib.initializeIntrinsics();
-// }
+static void perturb_camera_intrinsic(CameraGeometryPtr &camera) {
+  auto intrinsic = camera->getIntrinsicPtr();
+  intrinsic[0] += randf(0, 100);
+  intrinsic[1] += randf(0, 100);
+  intrinsic[2] += randf(0, 100);
+  intrinsic[3] += randf(0, 100);
+  intrinsic[4] = 0.0;
+  intrinsic[5] = 0.0;
+  intrinsic[6] = 0.0;
+  intrinsic[7] = 0.0;
+}
 
-// TEST(CalibCamera, initializeExtrinsic) {
-//   CalibCamera calib{TEST_CONFIG};
-//   calib.initializeIntrinsics();
-//   calib.initializeExtrinsics();
-// }
+static void perturb_camera_extrinsic(CameraGeometryPtr &camera) {
+  const auto phi = randf(-1.0, 1.0);
+  const auto theta = randf(-1.0, 1.0);
+  const auto psi = randf(-1.0, 1.0);
+  const auto quat = euler2quat(Vec3{phi, theta, psi});
 
-// TEST(CalibCamera, solve) {
-//   CalibCamera calib{TEST_CONFIG};
-//   calib.solve();
-//   calib.saveResults("/tmp/calib_camera-results.yaml");
-// }
+  auto extrinsic = camera->getExtrinsicPtr();
+  extrinsic[0] = randf(0, 0.5);
+  extrinsic[1] = randf(0, 0.5);
+  extrinsic[2] = randf(0, 0.5);
+  extrinsic[3] = quat.x();
+  extrinsic[4] = quat.y();
+  extrinsic[5] = quat.z();
+  extrinsic[6] = quat.w();
+}
+
+static void perturb_camera(CameraGeometryPtr &camera) {
+  perturb_camera_intrinsic(camera);
+  if (camera->getCameraId() != 0) {
+    perturb_camera_extrinsic(camera);
+  }
+}
 
 TEST(CalibCamera, solve) {
   CalibCamera calib;
@@ -44,7 +62,7 @@ TEST(CalibCamera, solve) {
 
   // Add camera data
   auto timeline = sim.get_timeline();
-  for (const auto &ts: timeline.timestamps) {
+  for (const auto &ts : timeline.timestamps) {
     for (const auto &event : timeline.getEvents(ts)) {
       if (auto target_event = dynamic_cast<CalibTargetEvent *>(event)) {
         const auto camera_id = target_event->camera_id;
@@ -54,6 +72,11 @@ TEST(CalibCamera, solve) {
     }
   }
 
+  // Perturb camera intrinsic and extrinsic
+  perturb_camera(calib.getCameraGeometry(0));
+  perturb_camera(calib.getCameraGeometry(1));
+
+  // Solve
   calib.solve();
 }
 
